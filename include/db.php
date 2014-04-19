9df7d9a9cf7d3adeeb56ae0ce04fbf4b87aa9740
Stefano Picozzi <StefanoPicozzi@gmail.com>
2014-04-19 21:58:58 +1000

    x

--- include/db.php
+++ include/db.php
@@ -7,19 +7,24 @@ if (! empty($client_gear_name) ) {
   
   #define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
   #define('DB_HOST', 'nudgeserver-spicozzi.rhcloud.com');
-  define('DB_HOST', '533673c04382eca3dd00061f-spicozzi.rhcloud.com');
+  #define('DB_HOST', '533673c04382eca3dd00061f-spicozzi.rhcloud.com');
+  define('DB_HOST', '535239b54382ec5cff00005c-spicozzi.rhcloud.com');
   
   #define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-  define('DB_PORT', '53396');
+  #define('DB_PORT', '53396');
+  define('DB_PORT', '44871');
 
   #define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-  define('DB_USER', 'adminXZyb64C');
+  #define('DB_USER', 'adminXZyb64C');
+  define('DB_USER', 'admins4SFfJZ');
 
   #define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-  define('DB_PASS', 'Xt4t23b_gLSt');
+  #define('DB_PASS', 'Xt4t23b_gLSt');
+  define('DB_PASS', 'IXpNxqRhElea');
 
   #define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
   #define('DB_NAME', 'nudgeserver');
+  #define('DB_NAME', 'nudgedb');
   define('DB_NAME', '');
 
 } else {
@@ -43,28 +48,33 @@ if (empty($mysql_hostname)) {
 $mysql_username = DB_USER;
 if (empty($mysql_username)) {
   #$mysql_username = 'root';
-  $mysql_username = 'adminXZyb64C';
+  #$mysql_username = 'adminXZyb64C';
+  $mysql_username = 'admins4SFfJZ';
+
 }
 
 /*** mysql password ***/
 $mysql_port = DB_PORT;
 if (empty($mysql_port)) {
-#   $mysql_port = '3306';
-   $mysql_port = '53396';
+   #$mysql_port = '3306';
+   #$mysql_port = '53396';
+   $mysql_port = '44871';
 }
 
 /*** mysql password ***/
 $mysql_password = DB_PASS;
 if (empty($mysql_password)) {
    #$mysql_password = '';
-   $mysql_password = 'Xt4t23b_gLSt';
+   #$mysql_password = 'Xt4t23b_gLSt';
+   $mysql_password = 'IXpNxqRhElea';
 }
 
 /*** database name ***/
 $mysql_dbname = DB_NAME;
 if (empty($mysql_dbname)) {
    #$mysql_dbname = 'nudge';
-   $mysql_dbname = 'nudgeserver';
+   #$mysql_dbname = 'nudgeserver';
+   $mysql_dbname = 'nudgedb';
 }
 
 ?>
